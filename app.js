396193617e6256ae5fe3c4e555b267175be553b1
Leo <leo_fan129albert@hotmail.com>
2018-05-30 18:05:53 -0500

    Archivo principal

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const route = require('./routes/index.js');
+const mongoose = require('mongoose');
+// const Task = require('../modelos/db');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/',route)
+
+app.use((req, res)=>{
+    res.status(404).send({url: req.originalUrl + ' not found'})
+  });
+
+app.listen(port, () => console.log('Example app listening on port',port));
